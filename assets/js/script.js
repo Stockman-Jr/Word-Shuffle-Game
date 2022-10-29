fa4e4b03a4a2e6cc4e7552af78ba8c189556265f
Stockman-Jr <stockmanmimmi@gmail.com>
2022-10-29 18:35:17 +0000

    Add words to arrays, clean up js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,11 +24,19 @@ let highscore = localStorage.getItem("highscore");
 
 // Word Arrays
 const easyWords = ['witch', 'zombie', 'skull', 'candy', 'ghost', 'fear', 'evil',
- 'moon', 'tomb', 'grim', 'blood', 'grave', 'spook', 'trick', 'treat'];
-const medWords = ['monkey', 'walrus', 'bobcat', 'jaguar', 'badger', 'hamster', 'weasel', 
-'beaver', 'donkey', 'raccoon', 'giraffe', 'buffalo', 'leopard', 'gorilla', 'dolphin'];
+  'moon', 'tomb', 'grim', 'blood', 'grave', 'spook', 'trick', 'treat', 'broom',
+  'bone', 'horns', 'demon', 'coven'
+];
+
+const medWords = ['monkey', 'walrus', 'bobcat', 'jaguar', 'badger', 'hamster', 'weasel',
+  'beaver', 'donkey', 'raccoon', 'giraffe', 'buffalo', 'leopard', 'gorilla', 'dolphin',
+  'rabbit', 'penguin', 'jackal', 'cougar', 'ostrich'
+];
+
 const hardWords = ['scarface', 'deadpool', 'superbad', 'hercules', 'godzilla', 'pinocchio',
- 'gladiator', 'zoolander', 'halloween', 'inception', 'wolverine', 'whiplash', 'precious', 'footloose', 'megamind', 'bewitched'];
+  'gladiator', 'zoolander', 'halloween', 'inception', 'wolverine', 'whiplash', 'footloose',
+  'megamind', 'bewitched', 'braveheart', 'zombieland', 'cinderella', 'armageddon', 'goodfellas'
+];
 
 
 /**
@@ -47,7 +55,7 @@ ruleBtn.addEventListener('click', function () {
 //Get the button elements and add event listeners
 document.addEventListener("DOMContentLoaded", function () {
 
-document.getElementById('high-score').innerHTML = localStorage.getItem("highscore");
+  document.getElementById('high-score').innerHTML = localStorage.getItem("highscore");
 
   startBtn.addEventListener("click", () => {
     chooseDifficulty();
@@ -61,18 +69,14 @@ function chooseDifficulty() {
   gameModeArea.classList.remove('hidden');
 
   for (let lvlBtn of lvlBtns) {
-
     lvlBtn.addEventListener("click", function () {
       let difficulty = this.getAttribute("data-type");
       if (difficulty === "easy") {
         gameMode = "easy";
-        console.log(`you chose ${gameMode}`);
       } else if (difficulty === "medium") {
         gameMode = "medium";
-        console.log(`you chose ${gameMode}`);
       } else if (difficulty === "hard") {
         gameMode = "hard";
-        console.log(`you chose ${gameMode}`);
       }
       startGame(gameMode);
     })
@@ -102,7 +106,6 @@ function shuffleWord(word) {
     let j = Math.floor(Math.random() * (i + 1));
     [letters[i], letters[j]] = [letters[j], letters[i]];
   }
-
   return letters.join("");
 }
 
@@ -114,15 +117,15 @@ function shuffleWord(word) {
 function winCondition(array) {
   var usedWord = chosenWord;
   var index = array.indexOf(usedWord);
-  console.log(index);
   if (index != -1) {
     array.splice(index, 1);
-    console.log(array);
-  } if (array.length === 14) {
-    console.log(score);
+  }
+  if (array.length === 12) {
     winGame();
   } else {
-    setTimeout(function(){ startGame(gameMode);}, 1000);
+    setTimeout(function () {
+      startGame(gameMode);
+    }, 1000);
   }
 
 }
@@ -141,8 +144,8 @@ function startGame(gameMode) {
 
   gameModeArea.classList.add('hidden');
   ruleBtn.classList.add('hidden');
-  nextLvlBtn.classList.add('hidden'); 
-  gameMsg.classList.add('hidden'); 
+  nextLvlBtn.classList.add('hidden');
+  gameMsg.classList.add('hidden');
 
   document.getElementById('heading').innerHTML = "Guess the word!";
 
@@ -155,7 +158,6 @@ function startGame(gameMode) {
     shuffledWord = shuffleWord(chosenWord);
     wordBox.innerHTML = shuffledWord;
     category.innerHTML = "Category: Halloween";
-    console.log(chosenWord, shuffledWord);
   } else if (gameMode === "medium") {
     chosenWord = getRandomWord(medWords);
     shuffledWord = shuffleWord(chosenWord);
@@ -168,8 +170,6 @@ function startGame(gameMode) {
     category.innerHTML = "Category: Movie Titles";
     nextLvlBtn.style.display = "none";
   }
-
-
 }
 
 /**
@@ -200,7 +200,6 @@ function checkAnswer() {
     userInput.value = "";
     userInput.focus();
   }
-
 }
 
 /**
@@ -212,7 +211,9 @@ function checkRemainingLives() {
   if (livesRemaining === 0) {
     gameOver();
   } else {
-    setTimeout(function(){wordBox.innerHTML = shuffledWord;}, 1000);
+    setTimeout(function () {
+      wordBox.innerHTML = shuffledWord;
+    }, 1000);
   }
 }
 
@@ -220,23 +221,22 @@ function checkRemainingLives() {
  * Checks for new high score and adds to local storage
  */
 
- function checkScore() {
-  console.log(score);
+function checkScore() {
   if (!localStorage.getItem("highscore")) {
     localStorage.setItem("highscore", score);
   }
-    if (score <= highscore) {
-     return;
-    } else if (score > highscore) {
-      document.getElementById('high-score').innerHTML = score;
-      localStorage.setItem("highscore", score);
-      console.log(highscore);
-    }
-  } 
+  if (score <= highscore) {
+    return;
+  } else if (score > highscore) {
+    document.getElementById('high-score').innerHTML = score;
+    localStorage.setItem("highscore", score);
+  }
+}
 
 /**
  * Displays a game over message for the user
- * Play again button resets game
+ * Checks score, highscore
+ * Play again button restarts game
  */
 
 function gameOver() {
@@ -247,17 +247,17 @@ function gameOver() {
   quitBtn.classList.add('hidden');
 
   document.getElementById('heading').innerHTML = "Game Over";
-  
+
   checkScore();
   document.getElementById('high-score').innerHTML = score;
 
   if (score <= parseInt(highscore) || highscore === null) {
     document.getElementById('message').innerHTML = `The word was ${chosenWord}. <br/> 
     You scored ${score} points!`;
-   } else if (score > parseInt(highscore)) {
+  } else if (score > parseInt(highscore)) {
     document.getElementById('message').innerHTML = `The word was ${chosenWord}. <br/> 
     New high-score!`;
-   }
+  }
 
   wordBox.innerHTML = chosenWord;
 
@@ -267,19 +267,22 @@ function gameOver() {
   playAgain.addEventListener('click', () => {
     document.location.reload()
   })
-
 }
+
+/**
+ * Displays a win message for the user
+ * Checks score, highscore
+ * Next level button takes you to next level unless you're in hard mode
+ */
 
 function winGame() {
   guessBtn.classList.add('hidden');
   userInput.classList.add('hidden');
   category.classList.add('hidden');
-  gameMsg.classList.remove('hidden'); 
+  gameMsg.classList.remove('hidden');
   nextLvlBtn.classList.remove('hidden');
-  
 
   checkScore();
-
   document.getElementById('high-score').innerHTML = score;
 
   document.getElementById('heading').innerHTML = "You won!";
@@ -287,20 +290,20 @@ function winGame() {
   if (score <= parseInt(highscore) || highscore === null) {
     document.getElementById('message').innerHTML = `Good job! You completed the ${gameMode} level! <br/> 
     You scored ${score} points!`;
-   } else if (score > parseInt(highscore)) {
+  } else if (score > parseInt(highscore)) {
     document.getElementById('message').innerHTML = `Good job! You completed the ${gameMode} level! <br/> 
     New high-score!`;
-   } 
+  }
 
-   nextLvlBtn.addEventListener('click', () => {  
-     if (gameMode === "easy") {     
-    gameMode = "medium";
-    startGame(gameMode);
-   } else if (gameMode === "medium") {
-     gameMode = "hard";
-     startGame(gameMode);
-   } 
-   })  
+  nextLvlBtn.addEventListener('click', () => {
+    if (gameMode === "easy") {
+      gameMode = "medium";
+      startGame(gameMode);
+    } else if (gameMode === "medium") {
+      gameMode = "hard";
+      startGame(gameMode);
+    }
+  })
 }
 
 //Event listeners for guess button, both mouseclick and keydown
@@ -313,9 +316,10 @@ guessBtn.addEventListener('click', () => {
   checkAnswer();
 })
 
+//Event listener for quit button, resets localstorage on hard mode
 quitBtn.addEventListener('click', () => {
   document.location.reload();
-  if(gameMode === "hard"){
+  if (gameMode === "hard") {
     localStorage.clear();
   }
 })
